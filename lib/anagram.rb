822050e28669af40592ee5bcf70f3ea457d0daff
Joshua Doud <bigjj3393@yahoo.com>
2023-02-06 18:44:12 -0800

    commit tree f04898f84e54ba2e728fa1d8ab5ab8deeefc6352

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+ class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match (words_array)
+        words_array.filter {|array_word| array_word.chars.sort == @word.chars.sort}
+    end
+           
+
+
+ end
